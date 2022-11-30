916f0c29a488d55a9b026a36a072a0a46333a333
Ponki-Ponki <vensop@gmail.com>
2022-11-30 17:09:37 +0300

    update itog_job

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿string[] arr = { "hello", "2", "world", "expention", "10", "4930", "-23", "ae" };
+
+
+string[] SortArrayText(string[] array)
+{
+    string[] arrSort = new string[array.Length];
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            arrSort[j++] = array[i];
+        }
+    }
+    return arrSort;
+}
+
+
+
+string[] newArray = SortArrayText(arr);
